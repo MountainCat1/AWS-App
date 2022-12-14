6b45709e747043c582f74914697448d8884c6172
MountainCat <tomasz.banas123@gmail.com>
2022-12-14 03:37:10 +0100

    test of env config in pipeline

--- message-board-frontend/src/App.tsx
+++ message-board-frontend/src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          {process.env.REACT_APP_API_URL}
         </p>
         <a
           className="App-link"
